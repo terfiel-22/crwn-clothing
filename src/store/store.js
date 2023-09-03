1315725242afb15240c73935f6f308fc97a72392
terfiel-22 <dietherfiel@gmail.com>
2023-09-03 18:15:01 +0800

    Add reselect library

--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,24 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
 } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+
+function logger(store) {
+  return function (next) {
+    return function (action) {
+      if (!action.type) {
+        return next(action);
+      }
+      console.log("type: ", action.type);
+      console.log("payload: ", action.payload);
+      console.log("currentState: ", store.getState());
+
+      next(action);
+      console.log("next state: ", store.getState());
+    };
+  };
+}
 
 const middlewares = [logger];
 
