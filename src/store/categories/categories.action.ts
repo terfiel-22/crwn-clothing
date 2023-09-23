25e00faed429ad9bf84d5bf20118fafa177eb449
terfiel-22 <dietherfiel@gmail.com>
2023-09-23 16:32:22 +0800

    Typing Categories

---
+++ src/store/categories/categories.action.ts
@@ -0,0 +1,38 @@
+import {
+  createAction,
+  Action,
+  ActionWithPayload,
+  withMatchmaker,
+} from "../../utils/reducer/reducer.utils";
+import { CATEGORIES_ACTION_TYPE, Category } from "./categories.types";
+
+export type FetchCategoriesStart =
+  Action<CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START>;
+
+export type FetchCategoriesSuccess = ActionWithPayload<
+  CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+  Category[]
+>;
+
+export type FetchCategoriesFailed = ActionWithPayload<
+  CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED,
+  Error
+>;
+
+export const fetchCategoriesStart = withMatchmaker(
+  (): FetchCategoriesStart =>
+    createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START)
+);
+
+export const fetchCategoriesSuccess = withMatchmaker(
+  (categoriesArray: Category[]): FetchCategoriesSuccess =>
+    createAction(
+      CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+      categoriesArray
+    )
+);
+
+export const fetchCategoriesFailed = withMatchmaker(
+  (error: Error): FetchCategoriesFailed =>
+    createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error)
+);
