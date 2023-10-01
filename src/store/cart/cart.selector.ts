edf3e545bb4fe7dabcc48cdd4ed17c0dcdef5b9c
terfiel-22 <dietherfiel@gmail.com>
2023-10-01 18:17:22 +0800

    Typing store.ts/root-reducer.ts/logger.ts

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,8 +1,9 @@
 import { createSelector } from "reselect";
 import { CartItem } from "./cart.types";
 import { CartState } from "./cart.reducer";
+import { RootState } from "../store";
 
-const selectCartReducer = (state): CartState => state.cart;
+const selectCartReducer = (state: RootState): CartState => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCartReducer],
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,17 +2,24 @@ import {
   compose,
   legacy_createStore as createStore,
   applyMiddleware,
+  Middleware,
 } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import createSagaMiddleware from "redux-saga";
 import { rootSaga } from "./root-saga";
 
+export type RootState = ReturnType<typeof rootReducer>;
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [!import.meta.env.PROD && logger, sagaMiddleware].filter(
-  Boolean
+  (middleware): middleware is Middleware => Boolean(middleware)
 );
 
 const composerEnhancer =
@@ -22,11 +29,16 @@ const composerEnhancer =
   compose;
 
 const composedEnhancers = composerEnhancer(applyMiddleware(...middlewares));
-const persistConfig = {
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+const persistConfig: ExtendedPersistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
 };
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = createStore(
   persistedReducer,
