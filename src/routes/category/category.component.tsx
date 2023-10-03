b68365b782bc8db59bb2a8db7eb4a53fa1309c9a
terfiel-22 <dietherfiel@gmail.com>
2023-10-03 17:20:33 +0800

    Typing Category

--- src/routes/category/category.component.tsx
+++ src/routes/category/category.component.tsx
@@ -10,8 +10,13 @@ import {
 } from "../../store/categories/categories.selector";
 import Spinner from "../../components/spinner/spinner.component";
 
+type CategoryRouteParams = {
+  category: string;
+};
 const Category = () => {
-  const { category } = useParams();
+  const { category } = useParams<
+    keyof CategoryRouteParams
+  >() as CategoryRouteParams;
   const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
